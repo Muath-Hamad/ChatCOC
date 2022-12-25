5e8d403280f7f521e7f7470551f7eb23c1e1274b
Muath-Hamad <muaath.jem@gmail.com>
2022-12-25 21:33:43 +0300

    Controller empty CRUD template

--- app/Http/Controllers/AdminfileController.php
+++ app/Http/Controllers/AdminfileController.php
@@ -7,4 +7,38 @@ use Illuminate\Http\Request;
 class AdminfileController extends Controller
 {
     //
+    public function index()
+    {
+        # code...
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function show()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
 }
--- app/Http/Controllers/UserfileController.php
+++ app/Http/Controllers/UserfileController.php
@@ -7,4 +7,39 @@ use Illuminate\Http\Request;
 class UserfileController extends Controller
 {
     //
+    public function index()
+    {
+        # code...
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function show()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
+
 }
