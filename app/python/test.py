4ebe02e6317dae247757fe21acb5147e2748dc62
Abdulmalik-rebdi <74616700+Abdulmalik-rebdi@users.noreply.github.com>
2023-01-08 13:01:02 +0300

    adding all possible returns from the class

--- app/python/test.py
+++ app/python/test.py
@@ -1,6 +1,3 @@
-import sys
-import json
-
 from gensim.models import word2vec
 import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
@@ -19,6 +16,7 @@ import re
 import spacy
 
 # Clean/Normalize Arabic Text
+
 def clean_str(text):
     search = ["أ","إ","آ","ة","_","-","/",".","،"," و "," يا ",'"',"ـ","'","ى","\\",'\n', '\t','"','?','؟','!']
     replace = ["ا","ا","ا","ه"," "," ","","",""," و"," يا","","","","ي","",' ', ' ',' ',' ? ',' ؟ ',' ! ']
@@ -43,8 +41,9 @@ def clean_str(text):
     text = text.strip()
 
     return text
-df = pd.read_csv('./app/python/data/class.csv')
-Qu = pd.read_excel('./app/python/data/Question.xlsx')
+Qu = pd.read_excel('./data/Question.xlsx')
+df = pd.read_csv('./data/class.csv')
+st = pd.read_csv('./data/StudentGPA.csv')
 trylist = list(df['المقرر'].unique())
 def split_digits_and_words(s):
     result = []
@@ -62,7 +61,6 @@ def split_digits_and_words(s):
     s = ''.join(str(x) for x in result)
 
     return s
-
 for i in range(len(trylist)):
     
     trylist[i] = split_digits_and_words(trylist[i])
@@ -74,12 +72,13 @@ for i in Qu['سوال']:
         quList.append(a)
         # print(a)
         count+=1
+li = list(df['المقرر'].unique())
 from scipy.spatial.distance import cosine
 import nltk
 
 #Load AraVec
-
-model = gensim.models.Word2Vec.load('./app/python/data/aravec/wikipedia_cbow_100')
+method = 0
+model = gensim.models.Word2Vec.load('./data/aravec/wikipedia_cbow_100')
 #import Dataset that have synonyms Question about the same thing 
 # A = pd.read_excel('./data/Question.xlsx')
 #converting the Dataframe into list 
@@ -88,84 +87,64 @@ model = gensim.models.Word2Vec.load('./app/python/data/aravec/wikipedia_cbow_100
 
 
 
+# this ia the use input 
+target = " اين مكان القاعه CS451" 
 
 
-###########################################################
-
-
-# this is the input from the user 
-
-target = "متى هو الموعد  المخصص لاختبار ماده math 115" 
 #spliting the sentence into words
+for word in li :
+    if word in target:
+        searchItem = word
+        method = 1
+        target = target.replace(word, "")
 
-#############################################################
-max = 0
-max1 = []
-for j in range(len(quList)):
-    sen1 = quList[j].split() #taking only the first Question in the list 'اين قاعة'
-    sen2 = target.split()
-    wordVec1 = 0
-    for i in range(len(sen1)):
-        #cleaning the word
-        try:
-            word = clean_str(sen1[i])
-        # adding the vectors of all words
-            wordVec1 += model.wv[ word]
-        except:
-            continue
-        
-    wordVec2 = 0
-    for i in range(len(sen2)):
-        try:
-            word = clean_str(sen2[i])
-            wordVec2 += model.wv[ word]
-        except:
-            continue
-    #finding the cosine similarity of the two sentences 
-    similarity = 1 - cosine(wordVec1, wordVec2)
-    if similarity > max :
-        max = similarity
-        max1 = sen1
-    # print(similarity)
+if method ==1 :
+    maxPer = 0
+    maxSen = []
+    for j in range(len(Qu['سوال'])):
+        sen1 = Qu['سوال'][j].split() #taking only the first Question in the list 'اين قاعة'
+        sen2 = target.split()
+        wordVec1 = 0
+        for i in range(len(sen1)):
+            #cleaning the word
+            try:
+                word = clean_str(sen1[i])
+            # adding the vectors of all words
+                wordVec1 += model.wv[ word]
+            except:
+                continue
+            
+        wordVec2 = 0
+        for i in range(len(sen2)):
+            try:
+                word = clean_str(sen2[i])
+                wordVec2 += model.wv[ word]
+            except:
+                continue
+        #finding the cosine similarity of the two sentences 
+        similarity = 1 - cosine(wordVec1, wordVec2)
+        if similarity > maxPer :
+            maxPer = similarity
+            maxSen = sen1
+            seachLoc = j
 
+# one of these ifs will be returned to the user 
+    
+if Qu.loc[seachLoc][1] == 1 :
+    # print(f"  وقت الاختبار في الفتره {list(df[df['المقرر'] == searchItem]['فتره'].unique())}")
+    for i in range(len(df[df['المقرر'] == searchItem]['الشعبة'])):
+        print(f" شعبه {list(df[df['المقرر'] == searchItem]['الشعبة'])[i] } في قاعه رقم {list(df[df['المقرر'] == searchItem]['القاعة'])[i]}")
+if Qu.loc[seachLoc][1] == 2 :
+    # print(f"  وقت الاختبار في الفتره {list(df[df['المقرر'] == searchItem]['فتره'].unique())}")
+    print(f"يدرسها {list(df[df['المقرر'] == searchItem]['المحاضر'].unique())}", end=u'', encoding='utf-8')
 
-
-## change these two output into json output
-##################################################
-print( max1)
-print( max)
-
-##################################################
-
-
-
-#x=sys.argv[1] # this is a system variable where a JSON will be passed from Laravel
-# y has sample JSON code that is tested and carries out to laravel if called from there
-
-y='''
-{
-"TestArray":[
-    {
-    "arg1": "test arg1",
-    "arg2": "test arg2"
-    },
-    {
-    "arg1": "test arg1",
-    "arg2": "test arg2"
-    }
-]
-}
-'''
+if Qu.loc[seachLoc][1] == 3 :
+    # print(f"  وقت الاختبار في الفتره {list(df[df['المقرر'] == searchItem]['فتره'].unique())}")
+    for i in range(len(df[df['المقرر'] == searchItem]['الشعبة'])):
+        print(f" شعبه {df[df['المقرر'] == searchItem]['الشعبة'][i] } تبدا من {df[df['المقرر'] == searchItem]['من'][i]} الى {df[df['المقرر'] == searchItem]['الى'][i]}")
+if Qu.loc[seachLoc][1] == 4 :
+    print(f"  وقت الاختبار في الفتره {list(df[df['المقرر'] == searchItem]['فتره'].unique())}")
 
 
-# data=json.loads(y) # loads method will take in a JSON code and decode it to be loaded in data python variable ..
-                    # data can be many types of variables depending on the passed JSON
-
-# print(json.dumps(data))
-
-data=json.loads(y) # loads method will take in a JSON code and decode it to be loaded in data python variable ..
-                    # data can be many types of variables depending on the passed JSON
-
-print(json.dumps(data))
 
 
