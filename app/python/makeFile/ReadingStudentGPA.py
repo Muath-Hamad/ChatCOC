ce3900d3638c06ccdabbb77695b7eae570f79b20
Muath-Hamad <muaath.jem@gmail.com>
2023-01-17 03:42:18 +0300

    File path handling

--- app/python/makeFile/ReadingStudentGPA.py
+++ app/python/makeFile/ReadingStudentGPA.py
@@ -12,9 +12,17 @@ import numpy as np
 import tabula
 
 # convert PDF into CSV file
-tabula.convert_into("../data/StudentGPA.pdf", "../data/StudentGPA.csv", output_format="csv", pages='all')
 
-df = pd.read_csv('../data/StudentGPA.csv',  on_bad_lines='skip')
+passed=sys.argv[1] # passed will have the file name that will be processed
+# "../data/StudentGPA.pdf" , ../data/StudentGPA.csv
+
+# set prefix path to file and append file name to the tail of the string
+inputtargetpath = "C:/xampp/htdocs/QU-Chatbot/storage/app/public/unprocessed_userfiles/" + passed
+outputtargetpath = "C:/xampp/htdocs/QU-Chatbot/storage/app/public/processed_userfiles/" + passed + ".csv"
+
+tabula.convert_into(inputtargetpath , outputtargetpath, output_format="csv", pages='all')
+
+df = pd.read_csv(outputtargetpath,  on_bad_lines='skip')
 
 df = df[df['رمز المقرر'] !='رمز المقرر' ]
 
@@ -31,4 +39,4 @@ for ind , row in df.iterrows() :
         except:
             pass
 
-df.to_csv('../data/StudentGPA.csv' , index=False)
+df.to_csv(outputtargetpath , index=False)
