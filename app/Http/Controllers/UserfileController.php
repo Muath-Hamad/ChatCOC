d0754e1c1f5741ca23133c8f84affd880135a813
Muath-Hamad <muaath.jem@gmail.com>
2023-01-17 04:00:43 +0300

    file name handling

--- app/Http/Controllers/UserfileController.php
+++ app/Http/Controllers/UserfileController.php
@@ -31,31 +31,36 @@ class UserfileController extends Controller
 
 
 
-        // $request->validate([
-        //     'userfile' => ['required','mimes:pdf','max:10000']
-        // ]);
-       // $user = Auth::user();
+        $request->validate([
+            'userfile' => ['required','mimes:pdf','max:10000']
+        ]);
+       $user = Auth::user();
 
-        //$curruserfile = new userfile();
+        $curruserfile = new userfile();
 
         $filepath=false;
-        // if($request->hasFile('userfile')){
+        if($request->hasFile('userfile')){
 
-        //     $filepath = $request->file('userfile')->storeAs(
-        //         'unprocessed_userfiles',
-        //         Auth::id().'_'. time() .'.'. $request->file('userfile')->getClientOriginalExtension(),
-        //         'public'
-        //     );
-        // }
+            $filename = Auth::id().'_'. time() .'.'. $request->file('userfile')->getClientOriginalExtension();
+            $request->file('userfile')->storeAs(
+                'unprocessed_userfiles',
+                 $filename,
+                'public'
+            );
+        }
 
-       // $data_to_JSON = array("path" => $filepath , "is_processed" => "false");
-        $data_to_JSON = array("user_request" => "متى هو الموعد  المخصص لاختبار ماده math 115");
 
-        $JSON_to_python = json_encode('math 115');
 
-        $script_path = app_path() . '\python\\' . 'test.py';
-        $command = "python3" . $script_path . $JSON_to_python;
-        $process = new Process(['C:\Python310\python.exe' , $script_path ,'math 115']);
+    //    // $data_to_JSON = array("path" => $filepath , "is_processed" => "false");
+    //     $data_to_JSON = array("user_request" => "متى هو الموعد  المخصص لاختبار ماده math 115");
+
+    //     $JSON_to_python = json_encode('math 115');
+
+
+
+        $script_path = app_path() . '\python\\' . '\makeFile\\'.'ReadingStudentGPA.py';
+        // $command = "python3" . $script_path . $filename;
+        $process = new Process(['C:\Python310\python.exe' , $script_path ,$filename]);
         $process->run();
 
         if(!$process->isSuccessful()){
