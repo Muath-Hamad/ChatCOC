f9ee7cd31f4426b0dfe9fd92ce30256ae931a7d2
badrsaleh1421 <badrdv9@gmail.com>
2023-01-17 18:31:46 +0300

    changes in home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" >
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel | Home</title>
@@ -46,7 +46,7 @@
 
                 @endif
     </div>
-    <span style="font-size:30px;cursor:pointer" class="m-3" onclick="openNav()">&#9776; </span>
+    <span style="font-size:30px;cursor:pointer" id="tlist" class="m-3" onclick="openNav()">&#9776; </span>
 
 
     <div class="container">
@@ -65,7 +65,7 @@
         </div>
         <div class="send">
           <!-- <img src="resources/_images/send.svg" alt="send" /> -->
-          <button type="button" class="btn btn-primary ml-4"> <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35"  fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+          <button type="button" class="btn  ml-4" id="bt" > <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35"  fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
             <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
             </svg>
             </button>
