4e24e25e0908833c97cddcdfb4e79c49cd50335a
Muath-Hamad <muaath.jem@gmail.com>
2023-01-19 08:08:08 +0300

    implemented integration throuh DB -- PHP side only

--- app/Http/Controllers/chatbotrequestController.php
+++ app/Http/Controllers/chatbotrequestController.php
@@ -14,10 +14,13 @@ class chatbotrequestController extends Controller
 
     public function create(Request $request)
     {
-        // dd($request->all());
-        $userinput = "";
+
+        $userinput = ""; // intialize variables
         $chatbot_response = "";
         $user_id = null;
+        $req_id = -1;
+        $process_succesful = true; // assume that proccess is succesful
+
         if(Auth::check()){ // check if user is logged in
             $user = Auth::user();
             $user_id = $user['id']; // store logged in user id
@@ -27,56 +30,39 @@ class chatbotrequestController extends Controller
 
           $userinput= $request->newdata;
 
+          // Request Logging ---
+            if($userinput->trim() != ""){ // verify that user input is not empty
+
+            $reqlog = new chatRequest();
+            $reqlog -> user_id = $user_id;
+            $reqlog -> ur_content = $userinput;
+            $reqlog -> cr_content = $chatbot_response;
+            $reqlog -> is_successful = false;
+            $reqlog -> save();
+
+            $req_id = $reqlog->id; // assign the added instance id to $req_id
+            }
          //return response($userinput);
-         //$data_to_JSON = array("user_request" => $userinput); // pput input into an array
-         // $JSON_to_python = json_encode($data_to_JSON); // encode array to JSON
           $script_path = app_path() . '\python\\' . 'chatbot.py'; // set up chatbot script path
-
-
-
           try{
-            $userinput = mb_convert_encoding($userinput , "UTF-8");
-            $process = new Process(['C:\Python38\python.exe' , $script_path ,$userinput]); // prepare process
-            $process->run(); // excute proccess
-            throw new ProcessFailedException($process);
+            $userinput = mb_convert_encoding($userinput , "UTF-8"); // encode $userinput to UTF-8
 
+            if($req_id != -1){ // ensure that instance was saved to DB
+                $process = new Process(['C:\Python38\python.exe' , $script_path ,$userinput , $req_id]); // prepare process
+                $process->run(); // excute proccess
+                throw new ProcessFailedException($process); }
           }catch(Exception $e){
             $error_msg = $e->getMessage();
+            $process_succesful = false;
             return response($error_msg);
           }
-          if($process->isSuccessful()){
-            $chatbot_response = $process->getOutput();
+
+          if($process_succesful && $req_id != -1){ // ensure that process is succesful & and the instance was created for the request
+            $result = chatRequest::find($req_id);
+            $chatbot_response = $result->cr_content;
             $chatbot_response = mb_convert_encoding($chatbot_response , "UTF-8");
             return response($chatbot_response);
-
           }
-
-
-
-
         }
-
-
-        // // Request Logging ---
-        // if($userinput != ""){ // verify that user input is not empty
-
-        // $user_decInput=json_decode($userinput); // decode json input to be stored as a string
-
-        // if($chatbot_response == ""){ // for now this will allow chatbot to response with Nothing -- if a static error message is to be returned this where it should be edited
-        //     $chatbot_decResponse = $chatbot_response;
-        // }else{
-        //     $chatbot_decResponse = json_decode($chatbot_response); // decode json response to be stored as a string
-        // }
-        // $reqlog = new chatRequest();
-        // $reqlog -> user_id = $user_id;
-        // $reqlog -> ur_content = $user_decInput;
-        // $reqlog -> cr_content = $chatbot_decResponse;
-        // $reqlog -> is_successful = false;
-
-        // }
-
-
-
     }
-
 }
