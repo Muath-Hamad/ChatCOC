7b0c7b44ef9f5a37fc75a01bb7a39fd9d9df6101
badrsaleh1421 <badrdv9@gmail.com>
2023-02-17 15:03:30 +0300

    css

--- resources/views/Admin/adminpage.blade.php
+++ resources/views/Admin/adminpage.blade.php
@@ -8,27 +8,27 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
+            <div class="  bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class=" p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="pr-20 max-w-xl">
 
                         @include('Admin.partials.upload-adminfile-form')
                     </div>
                 </div>
             </div>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class=" mt-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 {{-- <div class="p-6 text-gray-900">
                     {{ __("You're logged in as an admin!") }}
                 </div> --}}
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
+                <div class=" p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="pr-20 max-w-xl">
 
                         @include('Admin.partials.show-adminfile')
                     </div>
                 </div>
             </div>
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="mt-4 p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="pr-20 max-w-xl">
 
                     @include('Admin.partials.year-form')
                 </div>
